94f4a28b95a5a54657053d93f5386eb7b39c5b7f
vinctuan02 <vinctuan02@gmail.com>
2024-09-22 11:14:12 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    console.log("hello world");
+    console.log("hello worldgdsa");
     res.json('Hihihi')
-}) 
+})
 
 app.listen(8080, () => {
     console.log("App is running ......");
