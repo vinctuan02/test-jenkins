7fd7b7ff4e03182fe4c00f181ee95b4184d84b59
vinctuan02 <vinctuan02@gmail.com>
2024-09-22 11:17:43 +0700

    dsga

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    console.log("hello worldgdsa");
+    console.log("hello gdagddagworldgdsa");
     res.json('Hihihi')
 })
 
